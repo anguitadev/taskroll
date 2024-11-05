cb4fe570e33d5e29799be3490d311c848e1289aa
anguitadev <anguita.dev@gmail.com>
2024-11-05 18:04:13 +0200

    Página de carga

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center">
+			<span className="semibold text-2xl text-neutral-200">Cargando...</span>
+		</div>
+	);
+}
